f84b98a366bd927f18d76be30d5297059059b00a
Guilherme Alves <guilherme_ealves@outlook.com>
2022-08-12 22:52:33 -0300

    Alteração no caminho da imagem.

--- src/script/modal.js
+++ src/script/modal.js
@@ -6,7 +6,7 @@ const img = document.querySelector("#img");
 function ExibirModal(texto) {
     modal.classList.toggle("showModal");
     infoH1.innerText = texto;
-    img.src = `/src/img/Personal finance-amico.png`;
+    img.src = '../img/Personal finance-amico.png';
 }
 
 closeButton.addEventListener("click", ExibirModal);
